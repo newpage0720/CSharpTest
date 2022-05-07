991bca64d4394626d41c32c09b38bfbd39c88b0d
GR <GR@gmail.com>
2022-05-07 13:35:35 +0800

    0507test

---
+++ CSBasic_Week01.solution/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSBasic_Week01.solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
